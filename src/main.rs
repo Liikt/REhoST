5dbc4bd557aac1f57ecb289e987f5116b5ac3e00
Liikt <git@liikt.xyz>
2023-11-23 16:42:38 +0100

    add POC (execution not yet tested)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(start)]
+#![allow(non_snake_case)]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[start]
+fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    loop {}
 }
